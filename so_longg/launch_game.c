aaf72110cdfc101ee0b274b0d149ebc2f115604d
Sabin Ciurusniuc <saciurus@f2r4s18.clusters.42paris.fr>
2025-05-21 16:27:28 +0200

    fini

--- so_longg/launch_game.c
+++ so_longg/launch_game.c
@@ -6,7 +6,7 @@
 /*   By: saciurus <saciurus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 22:32:30 by saciurus          #+#    #+#             */
-/*   Updated: 2025/05/19 20:24:23 by saciurus         ###   ########.fr       */
+/*   Updated: 2025/05/21 16:25:50 by saciurus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,7 @@ int	launch_game(char **mapc, t_imgs *imgs, t_map *map)
 
 	data.mlx = mlx_init();
 	if (!data.mlx)
-		return (1);
+		return (ft_printf("Error\nÉchec de l'initialisation de la fenêtre\n"), 1);
 	define_sprites(data.mlx, imgs, map, mapc);
 	data.win = mlx_new_window(data.mlx, map->width * 64, map->height * 64,
 			"BABINSKI");
